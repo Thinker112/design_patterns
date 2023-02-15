8c261291a17a1e77a2b8ad4dfb12d1f66a3a7e30
yyb4j <17796553126@163.com>
2023-02-15 23:42:15 +0800

    (feat):责任链

--- behavioral/src/main/java/cn/yyb/behavioral/chain_of_responsibility/chain_of_responsibility01/Client.java
+++ behavioral/src/main/java/cn/yyb/behavioral/chain_of_responsibility/chain_of_responsibility01/Client.java
@@ -1,6 +1,7 @@
 package cn.yyb.behavioral.chain_of_responsibility.chain_of_responsibility01;
 
 /**
+ * 兽王大声命令他的军队。最近响应的是指挥官，然后是军官，然后是士兵。指挥官，军官，士兵这里就形成了一个责任链。
  * @author yyb
  * @create 2022-12-11 21:26
  */
